e6c89b85da6b3decf0df07aba1f6c9cf59774f77
xingwg <xing.weiguo@intellif.com>
2024-04-07 16:34:00 +0800

    🎨reformat

--- ort_custom_op/custom_op.cc
+++ ort_custom_op/custom_op.cc
@@ -10,89 +10,90 @@
 
 static GroupNormCustomOp c_GroupNormCustomOp;
 
-template <typename T>
+template<typename T>
 using ConstEigenVectorArrayMap = Eigen::Map<const Eigen::Array<T, Eigen::Dynamic, 1>>;
-template <typename T>
+template<typename T>
 using EigenVectorArrayMap = Eigen::Map<Eigen::Array<T, Eigen::Dynamic, 1>>;
 
-template <typename T>
-void GroupNormKernel<T>::Compute(OrtKernelContext* context) {
-  // Setup inputs
-  const OrtValue* input_X = ort_.KernelContext_GetInput(context, 0);
-  const T* X_data = reinterpret_cast<const T*>(ort_.GetTensorData<T>(input_X));
-  const OrtValue* input_num_groups = ort_.KernelContext_GetInput(context, 1);
-  const T* num_groups = reinterpret_cast<const T*>(ort_.GetTensorData<const T*>(input_num_groups));
-  int num_groups_i = int(num_groups[0]);
-  const OrtValue* input_scale = ort_.KernelContext_GetInput(context, 2);
-  const T* scale_data = reinterpret_cast<const T*>(ort_.GetTensorData<T>(input_scale));
-  const OrtValue* input_B = ort_.KernelContext_GetInput(context, 3);
-  const T* B_data = reinterpret_cast<const T*>(ort_.GetTensorData<T>(input_B));
+template<typename T>
+void GroupNormKernel<T>::Compute(OrtKernelContext *context) {
+    // Setup inputs
+    const OrtValue *input_X = ort_.KernelContext_GetInput(context, 0);
+    const T *X_data = reinterpret_cast<const T *>(ort_.GetTensorData<T>(input_X));
+    const OrtValue *input_num_groups = ort_.KernelContext_GetInput(context, 1);
+    const T *num_groups = reinterpret_cast<const T *>(ort_.GetTensorData<const T *>(input_num_groups));
+    int num_groups_i = int(num_groups[0]);
+    const OrtValue *input_scale = ort_.KernelContext_GetInput(context, 2);
+    const T *scale_data = reinterpret_cast<const T *>(ort_.GetTensorData<T>(input_scale));
+    const OrtValue *input_B = ort_.KernelContext_GetInput(context, 3);
+    const T *B_data = reinterpret_cast<const T *>(ort_.GetTensorData<T>(input_B));
 
-  // Setup output
-  OrtTensorDimensions dimensions(ort_, input_X);
-  OrtValue* output = ort_.KernelContext_GetOutput(context, 0, dimensions.data(), dimensions.size());
-  float* out = ort_.GetTensorMutableData<float>(output);
-  const int64_t N = dimensions[0];
-  const int64_t C = dimensions[1] / num_groups_i;  // assume [N C*num_groups H W]  per the spec
+    // Setup output
+    OrtTensorDimensions dimensions(ort_, input_X);
+    OrtValue *output = ort_.KernelContext_GetOutput(context, 0, dimensions.data(), dimensions.size());
+    float *out = ort_.GetTensorMutableData<float>(output);
+    const int64_t N = dimensions[0];
+    const int64_t C = dimensions[1] / num_groups_i;  // assume [N C*num_groups H W]  per the spec
 
-  OrtTensorTypeAndShapeInfo* output_info = ort_.GetTensorTypeAndShape(output);
-  ort_.ReleaseTensorTypeAndShapeInfo(output_info);
+    OrtTensorTypeAndShapeInfo *output_info = ort_.GetTensorTypeAndShape(output);
+    ort_.ReleaseTensorTypeAndShapeInfo(output_info);
 
-  // Do computation
-	int64_t sample_size = 1;
-  for (size_t i = 2; i < dimensions.size(); ++i) {
-    sample_size *= dimensions[i];
-  }
-  sample_size *= C;
+    // Do computation
+    int64_t sample_size = 1;
+    for (size_t i = 2; i < dimensions.size(); ++i) {
+        sample_size *= dimensions[i];
+    }
+    sample_size *= C;
 
-  for (auto i = 0; i < N * num_groups_i; ++i) {
-    ConstEigenVectorArrayMap<float> Xi(X_data + sample_size * i, sample_size);
-    const float Xi_mean = Xi.mean();
-    const float squared_norm = (Xi - Xi_mean).matrix().squaredNorm();
-    const float inv_stdev = 1.0f / std::sqrt(squared_norm / sample_size + epsilon_);
-    EigenVectorArrayMap<float> Yi(out + sample_size * i, sample_size);
-    const float channel_scale = inv_stdev * scale_data[i % (C * num_groups_i)];
-    const float channel_shift = B_data[i % (C * num_groups_i)] - Xi_mean * channel_scale;
-    Yi = Xi * channel_scale + channel_shift;
-  }
+    for (auto i = 0; i < N * num_groups_i; ++i) {
+        ConstEigenVectorArrayMap<float> Xi(X_data + sample_size * i, sample_size);
+        const float Xi_mean = Xi.mean();
+        const float squared_norm = (Xi - Xi_mean).matrix().squaredNorm();
+        const float inv_stdev = 1.0f / std::sqrt(squared_norm / sample_size + epsilon_);
+        EigenVectorArrayMap<float> Yi(out + sample_size * i, sample_size);
+        const float channel_scale = inv_stdev * scale_data[i % (C * num_groups_i)];
+        const float channel_shift = B_data[i % (C * num_groups_i)] - Xi_mean * channel_scale;
+        Yi = Xi * channel_scale + channel_shift;
+    }
 }
 
 
-
 struct OrtCustomOpDomainDeleter {
-  explicit OrtCustomOpDomainDeleter(const OrtApi* ort_api) {
-    ort_api_ = ort_api;
-  }
-  void operator()(OrtCustomOpDomain* domain) const {
-    ort_api_->ReleaseCustomOpDomain(domain);
-  }
+    explicit OrtCustomOpDomainDeleter(const OrtApi *ort_api) {
+        ort_api_ = ort_api;
+    }
 
-  const OrtApi* ort_api_;
+    void operator()(OrtCustomOpDomain *domain) const {
+        ort_api_->ReleaseCustomOpDomain(domain);
+    }
+
+    const OrtApi *ort_api_;
 };
 
 using OrtCustomOpDomainUniquePtr = std::unique_ptr<OrtCustomOpDomain, OrtCustomOpDomainDeleter>;
 static std::vector<OrtCustomOpDomainUniquePtr> ort_custom_op_domain_container;
 static std::mutex ort_custom_op_domain_mutex;
 
-static void AddOrtCustomOpDomainToContainer(OrtCustomOpDomain* domain, const OrtApi* ort_api) {
-  std::lock_guard<std::mutex> lock(ort_custom_op_domain_mutex);
-  auto ptr = std::unique_ptr<OrtCustomOpDomain, OrtCustomOpDomainDeleter>(domain, OrtCustomOpDomainDeleter(ort_api));
-  ort_custom_op_domain_container.push_back(std::move(ptr));
+static void AddOrtCustomOpDomainToContainer(OrtCustomOpDomain *domain, const OrtApi *ort_api) {
+    std::lock_guard<std::mutex> lock(ort_custom_op_domain_mutex);
+    auto ptr = std::unique_ptr<OrtCustomOpDomain, OrtCustomOpDomainDeleter>(domain, OrtCustomOpDomainDeleter(ort_api));
+    ort_custom_op_domain_container.push_back(std::move(ptr));
 }
 
-OrtStatus* ORT_API_CALL RegisterCustomOps(OrtSessionOptions* options, const OrtApiBase* api) {
-  OrtCustomOpDomain* domain = nullptr;
-  const OrtApi* ortApi = api->GetApi(ORT_API_VERSION);
+OrtStatus *ORT_API_CALL RegisterCustomOps(OrtSessionOptions *options, const OrtApiBase *api) {
+    OrtCustomOpDomain *domain = nullptr;
+    const OrtApi *ortApi = api->GetApi(ORT_API_VERSION);
 
-  if (auto status = ortApi->CreateCustomOpDomain("mydomain", &domain)) {
-    return status;
-  }
+    if (auto status = ortApi->CreateCustomOpDomain("mydomain", &domain)) {
+        return status;
+    }
 
-  AddOrtCustomOpDomainToContainer(domain, ortApi);;
-  if (auto status = ortApi->CustomOpDomain_Add(domain, &c_GroupNormCustomOp)) {
-    return status;
-  }
-  return ortApi->AddCustomOpDomain(options, domain);
+    AddOrtCustomOpDomainToContainer(domain, ortApi);;
+    if (auto status = ortApi->CustomOpDomain_Add(domain, &c_GroupNormCustomOp)) {
+        return status;
+    }
+    return ortApi->AddCustomOpDomain(options, domain);
 }
 
-template class GroupNormKernel<float>;
+template
+class GroupNormKernel<float>;
--- ort_custom_op/custom_op.h
+++ ort_custom_op/custom_op.h
@@ -2,6 +2,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 #pragma once
+
 #include <iostream>
 #include "onnxruntime_cxx_api.h"
 
@@ -9,7 +10,7 @@
 extern "C" {
 #endif
 
-ORT_EXPORT OrtStatus* ORT_API_CALL RegisterCustomOps(OrtSessionOptions* options, const OrtApiBase* api);
+ORT_EXPORT OrtStatus *ORT_API_CALL RegisterCustomOps(OrtSessionOptions *options, const OrtApiBase *api);
 
 #ifdef __cplusplus
 }
@@ -17,40 +18,45 @@ ORT_EXPORT OrtStatus* ORT_API_CALL RegisterCustomOps(OrtSessionOptions* options,
 
 
 struct Input {
-  const char* name;
-  std::vector<int64_t> dims;
-  std::vector<float> values;
+    const char *name;
+    std::vector<int64_t> dims;
+    std::vector<float> values;
 };
 
 struct OrtTensorDimensions : std::vector<int64_t> {
-  OrtTensorDimensions(Ort::CustomOpApi ort, const OrtValue* value) {
-    OrtTensorTypeAndShapeInfo* info = ort.GetTensorTypeAndShape(value);
-    std::vector<int64_t>::operator=(ort.GetTensorShape(info));
-    ort.ReleaseTensorTypeAndShapeInfo(info);
-  }
+    OrtTensorDimensions(Ort::CustomOpApi ort, const OrtValue *value) {
+        OrtTensorTypeAndShapeInfo *info = ort.GetTensorTypeAndShape(value);
+        std::vector<int64_t>::operator=(ort.GetTensorShape(info));
+        ort.ReleaseTensorTypeAndShapeInfo(info);
+    }
 };
 
-template <typename T>
+template<typename T>
 struct GroupNormKernel {
-	private:
-   float epsilon_;
-   Ort::CustomOpApi ort_;
+private:
+    float epsilon_;
+    Ort::CustomOpApi ort_;
 
-	public:
-  GroupNormKernel(Ort::CustomOpApi ort, const OrtKernelInfo* info) : ort_(ort) {
-    epsilon_ = ort_.KernelInfoGetAttribute<float>(info, "epsilon");
-  }
+public:
+    GroupNormKernel(Ort::CustomOpApi ort, const OrtKernelInfo *info) : ort_(ort) {
+        epsilon_ = ort_.KernelInfoGetAttribute<float>(info, "epsilon");
+    }
 
-  void Compute(OrtKernelContext* context);
+    void Compute(OrtKernelContext *context);
 };
 
 struct GroupNormCustomOp : Ort::CustomOpBase<GroupNormCustomOp, GroupNormKernel<float>> {
-  void* CreateKernel(Ort::CustomOpApi api, const OrtKernelInfo* info) const { return new GroupNormKernel<float>(api, info); };
-  const char* GetName() const { return "CustomGroupnorm"; };
+    void *CreateKernel(Ort::CustomOpApi api, const OrtKernelInfo *info) const {
+        return new GroupNormKernel<float>(api, info);
+    };
 
-  size_t GetInputTypeCount() const { return 4; };
-  ONNXTensorElementDataType GetInputType(size_t /*index*/) const { return ONNX_TENSOR_ELEMENT_DATA_TYPE_FLOAT; };
+    const char *GetName() const { return "CustomGroupnorm"; };
 
-  size_t GetOutputTypeCount() const { return 1; };
-  ONNXTensorElementDataType GetOutputType(size_t /*index*/) const { return ONNX_TENSOR_ELEMENT_DATA_TYPE_FLOAT; };
+    size_t GetInputTypeCount() const { return 4; };
+
+    ONNXTensorElementDataType GetInputType(size_t /*index*/) const { return ONNX_TENSOR_ELEMENT_DATA_TYPE_FLOAT; };
+
+    size_t GetOutputTypeCount() const { return 1; };
+
+    ONNXTensorElementDataType GetOutputType(size_t /*index*/) const { return ONNX_TENSOR_ELEMENT_DATA_TYPE_FLOAT; };
 };
