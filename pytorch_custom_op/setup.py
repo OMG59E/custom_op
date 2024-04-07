78a7c9a7c1364ae6b5714e75829a23822b9beda4
xingwg <xing.weiguo@intellif.com>
2024-04-07 15:24:19 +0800

    🔧modify eigen3 inc

--- pytorch_custom_op/setup.py
+++ pytorch_custom_op/setup.py
@@ -1,10 +1,13 @@
 # SPDX-License-Identifier: Apache-2.0
-
+import os
 from setuptools import setup
 from torch.utils import cpp_extension
 
+filepath = os.path.dirname(os.path.abspath(__file__))
+eigen3_inc = os.path.join(filepath, "../3rdparty/eigen3")
+
 setup(name="custom_group_norm",
       ext_modules=[cpp_extension.CppExtension("custom_group_norm", ["custom_group_norm.cpp"], 
-                                              include_dirs=["/usr/local/include/eigen3"])],
+                                              include_dirs=[eigen3_inc])],
       license="Apache License v2.0",
       cmdclass={"build_ext": cpp_extension.BuildExtension})
